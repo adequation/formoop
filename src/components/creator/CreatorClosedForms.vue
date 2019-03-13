72f8194d8a8a0e175ad6ec0c323d001d08f43fae
quentin <quentin.codelupi@hotmail.fr>
2019-03-13 12:38:09 +0100

    [fix] CreatorClosedForm Json Parsing when not answered

--- src/components/creator/CreatorClosedForms.vue
+++ src/components/creator/CreatorClosedForms.vue
@@ -35,49 +35,55 @@
 
           Object.keys(BrutForm.entries).forEach((entrykey) => {
             let answers = [];
+            if(BrutForm.usersAnswers !== undefined) {
 
-            if (BrutForm.entries[entrykey].type === 'radio' || BrutForm.entries[entrykey].type === 'select'){
-              answers = BrutForm.entries[entrykey].answer.answers;
-              answers.forEach(answer => {
-                answer["nbanswer"] = Object.keys(BrutForm.usersAnswers[entrykey]).filter(UAkey => BrutForm.usersAnswers[entrykey][UAkey] === answer.id).length;
-              });
-            }
-            if(BrutForm.entries[entrykey].type === 'checkbox'){
-              answers = BrutForm.entries[entrykey].answer.answers;
-              answers.forEach(answer => {
-                answer["nbanswer"] = 0;
-                Object.keys(BrutForm.usersAnswers[entrykey]).forEach(idUser => {
-                  console.log(BrutForm.usersAnswers[entrykey][idUser]);
-                  answer["nbanswer"] += BrutForm.usersAnswers[entrykey][idUser].includes(answer.id)? 1:0;
-                });
-              });
-            }
-            if(BrutForm.entries[entrykey].type === 'textarea' || BrutForm.entries[entrykey].type === 'text'){
-              Object.keys(BrutForm.usersAnswers[entrykey]).forEach(userID => answers.push(BrutForm.usersAnswers[entrykey][userID]));
-            }
+                if (BrutForm.entries[entrykey].type === 'radio' || BrutForm.entries[entrykey].type === 'select') {
+                  answers = BrutForm.entries[entrykey].answer.answers;
+                  answers.forEach(answer => {
+                    if (BrutForm.usersAnswers[entrykey] != null)
+                      answer["nbanswer"] = Object.keys(BrutForm.usersAnswers[entrykey]).filter(UAkey => BrutForm.usersAnswers[entrykey][UAkey] === answer.id).length;
+                  });
+                }
+                if (BrutForm.entries[entrykey].type === 'checkbox') {
+                  answers = BrutForm.entries[entrykey].answer.answers;
+                  answers.forEach(answer => {
+                    answer["nbanswer"] = 0;
+                    if (BrutForm.usersAnswers[entrykey] != null)
+                      Object.keys(BrutForm.usersAnswers[entrykey]).forEach(idUser => {
+                        answer["nbanswer"] += BrutForm.usersAnswers[entrykey][idUser].includes(answer.id) ? 1 : 0;
+                      });
+                  });
+                }
+                if (BrutForm.entries[entrykey].type === 'textarea' || BrutForm.entries[entrykey].type === 'text') {
+                  if (BrutForm.usersAnswers[entrykey] != null)
+                    Object.keys(BrutForm.usersAnswers[entrykey]).forEach(userID => answers.push(BrutForm.usersAnswers[entrykey][userID]));
+                }
 
-            newEntries.push(
-              {
-                title: BrutForm.entries[entrykey].question.title,
-                type: BrutForm.entries[entrykey].type,
-                answers: answers,
-              });
+                newEntries.push(
+                  {
+                    title: BrutForm.entries[entrykey].question.title,
+                    type: BrutForm.entries[entrykey].type,
+                    answers: answers,
+                  });
+            }
           });
 
           Object.keys(BrutForm.users).forEach((userkey) => {
 
-            let givenAnswers = [];
 
-            Object.keys(BrutForm.usersAnswers).forEach(formID =>
+            if (BrutForm.usersAnswers){
+              let givenAnswers = [];
+
+              Object.keys(BrutForm.usersAnswers).forEach(formID =>
               Object.keys(BrutForm.usersAnswers[formID]).forEach(userID => {
                 if (userID === userkey)
-                  if(BrutForm.entries[formID].type === 'radio' || BrutForm.entries[formID].type === 'select'){
+                  if (BrutForm.entries[formID].type === 'radio' || BrutForm.entries[formID].type === 'select') {
                     givenAnswers.push({
                       question: BrutForm.entries[formID].question.title,
                       answer: BrutForm.entries[formID].answer.answers.filter(answer => answer.id === BrutForm.usersAnswers[formID][userID])[0].text,
                     });
                   }
-                if(BrutForm.entries[formID].type ==='checkbox'){
+                if (BrutForm.entries[formID].type === 'checkbox') {
                   let answer = [];
                   BrutForm.usersAnswers[formID][userID].forEach(A => {
                     answer.push(BrutForm.entries[formID].answer.answers.find(QA => QA.id === A).text)
@@ -87,7 +93,7 @@
                     answer: answer,
                   });
                 }
-                if(BrutForm.entries[formID].type === 'textarea' || BrutForm.entries[formID].type === 'text'){
+                if (BrutForm.entries[formID].type === 'textarea' || BrutForm.entries[formID].type === 'text') {
                   givenAnswers.push({
                     question: BrutForm.entries[formID].question.title,
                     answer: BrutForm.usersAnswers[formID][userID],
@@ -101,6 +107,7 @@
               name: BrutForm.users[userkey].name,
               givenAnswers: givenAnswers,
             })
+          }
           });
 
           return {
