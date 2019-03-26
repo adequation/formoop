f886350866ce10ffab5c7bca44fc7b51c6c99036
Steeve Ciminera <ciminera.steeve@gmail.com>
2019-03-26 19:02:02 +0100

    added basic parsing

---
+++ src/components/creator/CSVImportModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="csv-import-modal">
+
+    <Modal v-if="showModal" @close="closeModal">
+      <h1 slot="header">Génération du formulaire</h1>
+
+      <div slot="body">
+        <p>Fichier de génération :
+          <CSVParser/>
+        </p>
+
+        <div v-if="publishable">
+
+          <p>Publication et création de {{Object.keys(importedEntities).length}} formulaires</p>
+
+
+
+          <button type="button" @click="generateAndPublishForms">Générer !</button>
+        </div>
+
+        <div v-else-if="!publishable && errors">
+          ERREURS
+        </div>
+
+      </div>
+    </Modal>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "CSVImportModal"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/general/CSVParser.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="json-parser">
+    <input type="file" accept=".csv" @change="onChangeCSVInput($event)"/>
+    <div v-if="errors">
+      <p>{{errors}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {parseCSV, parsePromoteurCSV} from "@/helpers/csvParserHelpers";
+
+  export default {
+        name: "CSVParser",
+      data(){
+        return {
+          parsedData : null,
+          errors: ''
+        }
+      },
+      props: {
+      },
+      methods: {
+        onChangeCSVInput(e) {
+          const reader = new FileReader();
+          reader.readAsText(e.target.files[0], "UTF-8");
+
+          reader.onload = (evt) => {
+            const rawFile = evt.target.result;
+            this.parsedData = parsePromoteurCSV(rawFile);
+            console.log(JSON.stringify(this.parsedData, true));
+            this.$root.$emit("csv-parsed", this.parsedData);
+          };
+
+          reader.onerror = function (evt) {
+            this.errors = evt.target;
+          }
+
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
