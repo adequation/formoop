5f681d5cffb9210207343dd0f7a6492e6bb3a1db
Sancho <sanchezromain74@gmail.com>
2019-04-12 15:50:22 +0200

    Small fix on select campaign component

--- src/components/creator/CreatorCampaignSelect.vue
+++ src/components/creator/CreatorCampaignSelect.vue
@@ -163,6 +163,8 @@
         this.add(data);
       });
 
+      this.selectedCampaigns = this.formCampaign;
+
       this.$root.$on('remove-from-selected-by-user', (data) => {
         this.remove(data);
       });
